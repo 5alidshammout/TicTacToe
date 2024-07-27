d4b792bdf0abd8876053acc18464271333f59f19
5alidshammout <5alidshammout@gmail.com>
2024-07-27 07:15:14 +0300

    scss + start adding functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,25 @@
+import { useState, MouseEvent} from "react";
 import Square from "./square";
 
+let players = ["O", "X"]
+
+function placeSign(e:MouseEvent, turn:string, setTurn:React.Dispatch<React.SetStateAction<string>>) {
+  (e.target as HTMLDivElement).innerText = turn
+  setTurn(players[Math.abs(players.indexOf(turn) - 1)])
+}
+
 function App() {
+  const [turn, setTurn] = useState("X");
   return (
     <>
-      <h1 className="text-slate-400 font-black text-xl">It's X turn</h1>
+      <h1 className="text-slate-400 font-black text-xl">It's {turn} turn</h1>
       <div id="board" className="grid grid-cols-3 grid-rows-3">
         {[...Array(9)].map((_, i) => (
           <Square
             key={i}
             index={i}
-            selected={(e) => console.log(i, e)}
+            player=""
+            selected={(e) => placeSign(e, turn, setTurn)}
           ></Square>
         ))}
       </div>
--- src/components/square.tsx
+++ src/components/square.tsx
@@ -3,22 +3,25 @@ import { MouseEvent } from "react";
 interface props {
   index: number;
   selected: (e: MouseEvent) => void;
+  player: "O" | "X" | "";
 }
 
-function Square({ index, selected }: props) {
+function Square({ index, selected, player }: props) {
   let [row, col] = [(index % 3) + 1, Math.floor(index / 3) + 1];
   return (
     <>
       <div
         className={
-          "size-20 border-slate-400 " +
+          "size-20 border-slate-400 flex items-center justify-center text-4xl " +
           (row !== 1 ? "border-l-4 " : "") +
           (row !== 3 ? "border-r-4 " : "") +
           (col !== 1 ? "border-t-4 " : "") +
           (col !== 3 ? "border-b-4 " : "")
         }
         onClick={(e) => selected(e)}
-      ></div>
+      >
+        <h1>{player}</h1>
+      </div>
     </>
   );
 }

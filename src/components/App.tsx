4a14ab2627db31404697b442c28fe731d7548eac
5alidshammout <5alidshammout@gmail.com>
2024-07-29 08:49:43 +0300

    add winning functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,36 @@ import Square from "./square";
 
 let players = ["O", "X"];
 
+function checkWinner(
+  rowi: number,
+  coli: number,
+  turn: string,
+  Squares: string[][]
+) {
+  let coord = [rowi, coli];
+  if (Squares[rowi].every((el) => el === turn)) {
+    return turn;
+  } else if (Squares.map((row) => row[coli]).every((el) => el === turn)) {
+    return turn;
+  } else if (
+    !(
+      coord.filter((x) => x === 1).length === 1 &&
+      coord.filter((x) => x !== 1).length === 1
+    )
+  ) {
+    let d0: string[] = [];
+    let d1: string[] = [];
+    for (let i = 0; i < 3; i++) {
+      d0.push(Squares[i][i]);
+      d1.push(Squares[2 - i][i]);
+    }
+    if ([d0, d1].some((arr) => arr.every((el) => el === turn))) {
+      return turn;
+    }
+  }
+  return "";
+}
+
 function placeSign(
   coli: number,
   rowi: number,
@@ -12,16 +42,24 @@ function placeSign(
   Squares: string[][],
   setSquares: React.Dispatch<React.SetStateAction<string[][]>>
 ) {
-  if (val !== "") {
+  if (val !== "" || turn[0] === "-") {
     return;
   }
   let tmp = Squares.map((r) => [...r]);
   tmp[rowi][coli] = turn;
   setSquares(tmp);
-  if (tmp.some((x) => x.some((y) => y === ""))) {
-    setTurn(players[Math.abs(players.indexOf(turn) - 1)]);
+
+  console.log(rowi, coli, turn, tmp);
+  let winner = checkWinner(rowi, coli, turn, tmp);
+
+  if (winner !== "") {
+    setTurn(`-${turn}`);
   } else {
-    setTurn("");
+    if (tmp.some((x) => x.some((y) => y === ""))) {
+      setTurn(players[Math.abs(players.indexOf(turn) - 1)]);
+    } else {
+      setTurn("");
+    }
   }
 }
 
@@ -34,7 +72,11 @@ function App() {
   return (
     <>
       <h1 className="text-slate-400 font-black text-xl">
-        {turn === "" ? "Game over" : `It's ${turn} turn`}
+        {turn === ""
+          ? "Game over"
+          : turn[0] === "-"
+          ? `${turn[1]} WON!`
+          : `It's ${turn} turn`}
       </h1>
       <div id="board" className="grid grid-cols-3 grid-rows-3">
         {squares.map((row, rowi) =>
